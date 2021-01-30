7643962f612f0bcdcda3fad14d637ec231b305e2
Nikita <nneewwdd@gmail.com>
2021-01-30 22:13:03 +0300

    first commit

---
+++ JS/script.js
@@ -0,0 +1,69 @@
+"use strict";
+
+// let number = 4.6;
+
+// console.log(5*4);
+// console.log('string');
+
+// const persone = '5';
+// const bool = true;
+
+// const obj = {
+//     name: "Nikita",
+//     age: 25,
+//     isMarrid: false
+// };
+
+// console.log(obj.isMarrid);
+
+// const arr = ['govno.png', 'apple.jpg', 'cc', 6, 'lol.bmp', []];
+// console.log(arr[0]);
+
+// const max = Number.MAX_SAFE_INTEGER;
+// console.log(max);
+
+// alert("Hallo");
+
+// const result = confirm("Are you hire?");
+// console.log(result);
+
+// const infiniti = +prompt("Вам есть 18?", "18");
+// console.log(typeof(infiniti));
+
+// const answers = [];
+// answers[0] = prompt("Как ваше имя?", "");
+// answers[1] = prompt("как ваша фамилия?", "");
+// answers[2] = prompt("Сколько тебе лет?", "");
+
+// console.log(typeof(answers));
+
+// const ppp = alert('Х');
+// prompt('ddd?', 'q');
+
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`);   
+
+// const user = 'Nikitos';
+// alert(`Привет, ${user}`);
+
+// console.log(4 + "5");
+
+let incr = 10,
+    decr = 10;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2 + 2 * 2 != '8');
+
+const isChekced = false,
+      isClose = false;
+
+console.log(isChekced || !isClose);
+
+
+
+
+
